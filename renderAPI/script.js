b41b7f6884d8d7cf212746bc877d9c21aa53117b
arpitasi1gh <arpitasi1gh@gmail.com>
2026-03-09 15:49:09 +0530

    change weatherAPI Fetch to cardsAPI Fetch

--- renderAPI/script.js
+++ renderAPI/script.js
@@ -1,35 +1,30 @@
-let btn = document.querySelector("button");
-btn.addEventListener("click", function() {
-    let input = document.querySelector("#cityInput");
-    let CITY = input.value;
-    let API_KEY = "32a09b92acaf9b09442e9baca05fe7db";
-    let API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`;
-    
-    fetch(API_URL)
-    .then(response => {return response.json()})
-    .then(data => {
-        console.log(data);
-        let container = document.querySelector(".renderAPI");
-        let box = document.createElement("div");
-        box.setAttribute("class", "box");
-        let h = document.createElement("h2");
-        h.textContent = data.name;
-        let p = document.createElement("p");
-        p.textContent = `${data.main.temp}°C \n ${data.weather[0].description}`;
-        container.innerHTML = "";
-        container.appendChild(box);
-        box.append(h, p);
-    })
-    .catch(err => {
-        console.log(err);
-        let container = document.querySelector(".renderAPI");
-        let box = document.createElement("div");
-        box.setAttribute("class", "box");
-        let h = document.createElement("h2");
-        h.textContent = "Error fetching weather data";
-        container.innerHTML = "";
-        container.appendChild(box);
-        box.append(h);
+let main = document.querySelector(".main");
+let cards = document.querySelector(".cards-container");
+let error = document.querySelector("error-container");
+
+fetch("https://fakestoreapi.com/products")
+.then(result => {
+    return result.json();
+})
+.then(data => {
+    console.log(data);
+
+    data.forEach(ele => {
+        let card = document.createElement("div");
+        card.setAttribute("class", "card");
+        card.innerHTML = `
+        <h1>${ele.title}</h1>
+        <img src=${ele.image}>
+        <div><strong>Category:</strong> ${ele.category}</div>
+        <div><strong>Price:</strong> ${ele.price}</div>
+        <div><strong>Description:</strong> ${ele.description}</div>
+        <div><strong>Rating:</strong> ${ele.rating.rate}</div>
+        <div><strong>Review Count:</strong> ${ele.rating.count}</div>
+        `;
+        document.body.append(card);
     });
-    
-});
+})
+.catch(error => {
+    console.log(error);
+}
+)
