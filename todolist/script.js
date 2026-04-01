2891791bf796e3a5a491fce99ad368bcb25e8084
arpitasi1gh <arpitasi1gh@gmail.com>
2026-04-01 23:56:55 +0530

    add todolist

---
+++ todolist/script.js
@@ -0,0 +1,16 @@
+let list = document.getElementsById("my-list");
+let taskBtnsDiv = document.querySelector(".task-btns");
+
+let taskTitleInput = document.getElementById("task-title-input");
+let taskDetailsInput = document.getElementById("task-details-input");
+let taskDueTimeInput = document.getElementById("task-due-time-input");
+
+let markCompleteBtn = document.getElementById("markataskascomplete-btn");
+let editBtn = document.getElementById("editatask-btn");
+let deleteBtn = document.getElementById("deleteatask-btn");
+let saveBtn = document.getElementsById("saveatask-btn");
+let addBtn = document.getElementById("addatask-btn");
+
+addBtn.addEventListener("click", () => {
+    
+});
